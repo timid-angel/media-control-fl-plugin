892386ec582bc1a3ed0d2ecd551670160e6168be
Nathan M.S <natms24@gmail.com>
2024-09-05 17:20:57 +0300

    Add query functionality for the command

--- main.py
+++ main.py
@@ -8,61 +8,71 @@ sys.path.append(os.path.join(parent_folder_path, 'plugin'))
 from flowlauncher import FlowLauncher
 import webbrowser
 
+COMMAND_NAMES = {
+    "pause_or_resume_media": "pause/resume",
+    "next_media": "next",
+    "prev_media": "prev",
+    "stop_media": "stop"
+}
+
+COMMANDS = [
+        {
+            "title": COMMAND_NAMES["pause_or_resume_media"],
+            "subTitle": "Pause or resume playing media",
+            "icoPath": "images/app.png",
+            "score": 1,
+            "jsonRPCAction": {
+                "method": "pause_resume_media",
+                "parameters": []
+            }
+        },
+        {
+            "title": COMMAND_NAMES["next_media"],
+            "subTitle": "Plays next media if it is available",
+            "icoPath": "images/app.png",
+            "score": 1,
+            "jsonRPCAction": {
+                "method": "next_media",
+                "parameters": []
+            }
+        },
+        {
+            "title": COMMAND_NAMES["prev_media"],
+            "subTitle": "Plays previous media if it is available",
+            "icoPath": "images/app.png",
+            "score": 1,
+            "jsonRPCAction": {
+                "method": "prev_media",
+                "parameters": []
+            }
+        },
+        {
+            "title": COMMAND_NAMES["stop_media"],
+            "subTitle": "Stops playing media",
+            "icoPath": "images/app.png",
+            "score": 1,
+            "jsonRPCAction": {
+                "method": "stop_media",
+                "parameters": []
+            }
+        }
+    ]
+
 class Nutils(FlowLauncher):
 
-    def query(self, query):
-        return [
-            {
-                "title": "Hello World, this is where title goes. {}".format(('Your query is: ' + query, query)[query == '']),
-                "subTitle": "This is where your subtitle goes, press enter to open Flow's url",
-                "icoPath": "Images/app.png",
-                "jsonRPCAction": {
-                    "method": "open_url",
-                    "parameters": ["https://github.com/Flow-Launcher/Flow.Launcher"]
-                },
-                "score": 1
-            },
-            {
-                "title": "pause/resume",
-                "subTitle": "Pause or resume playing media",
-                "icoPath": "images/app.png",
-                "score": 1,
-                "jsonRPCAction": {
-                    "method": "pause_resume_media",
-                    "parameters": []
-                }
-            },
-            {
-                "title": "next",
-                "subTitle": "Plays next media if it is available",
-                "icoPath": "images/app.png",
-                "score": 1,
-                "jsonRPCAction": {
-                    "method": "next_media",
-                    "parameters": []
-                }
-            },
-            {
-                "title": "prev",
-                "subTitle": "Plays previous media if it is available",
-                "icoPath": "images/app.png",
-                "score": 1,
-                "jsonRPCAction": {
-                    "method": "prev_media",
-                    "parameters": []
-                }
-            },
-            {
-                "title": "stop",
-                "subTitle": "Stops playing media",
-                "icoPath": "images/app.png",
-                "score": 1,
-                "jsonRPCAction": {
-                    "method": "stop_media",
-                    "parameters": []
-                }
-            }
-        ]
+    def query(self, query: str) -> list:
+        if len(query) == 0:
+            return []
+        
+        return self.resolve_query(query.strip().lower())
+
+    def resolve_query(self, arg: str) -> list:        
+        query_result = []
+        for command in COMMANDS:
+            if arg in command["title"]:
+                query_result.append(command)
+            
+        return query_result
 
     def context_menu(self, data):
         return [
