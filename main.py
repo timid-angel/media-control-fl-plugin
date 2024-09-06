88d3a2dc592d018d131fdac880a5ddc2881a5e3f
Nathan M.S <natms24@gmail.com>
2024-09-06 15:41:30 +0300

    Refactor query resolution to enable params

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
-from pathlib import Path
-import sys,os
+import pyautogui
+import webbrowser
+from flowlauncher import FlowLauncher
+from math import ceil
+import sys
+import os
 parent_folder_path = os.path.abspath(os.path.dirname(__file__))
 sys.path.append(parent_folder_path)
 sys.path.append(os.path.join(parent_folder_path, 'lib'))
 sys.path.append(os.path.join(parent_folder_path, 'plugin'))
 
-from flowlauncher import FlowLauncher
-import webbrowser
-import pyautogui
 
 KEYBOARD_MAPPING = {
     "pause/resume": "playpause",
@@ -29,93 +30,100 @@ COMMAND_NAMES = {
     "mute": "mute",
 }
 
-COMMANDS = [
-        {
-            "title": COMMAND_NAMES["pause_or_resume_media"],
-            "subTitle": "Pause or resume playing media",
-            "icoPath": "images/app.png",
-            "score": 1,
-            "jsonRPCAction": {
-                "method": "pause_or_resume_media",
-                "parameters": []
-            }
-        },
-        {
-            "title": COMMAND_NAMES["next_media"],
-            "subTitle": "Plays next media if it is available",
-            "icoPath": "images/app.png",
-            "score": 1,
-            "jsonRPCAction": {
-                "method": "next_media",
-                "parameters": []
-            }
-        },
-        {
-            "title": COMMAND_NAMES["prev_media"],
-            "subTitle": "Plays previous media if it is available",
-            "icoPath": "images/app.png",
-            "score": 1,
-            "jsonRPCAction": {
-                "method": "prev_media",
-                "parameters": []
-            }
-        },
-        {
-            "title": COMMAND_NAMES["stop_media"],
-            "subTitle": "Stops playing media",
-            "icoPath": "images/app.png",
-            "score": 1,
-            "jsonRPCAction": {
-                "method": "stop_media",
-                "parameters": []
-            }
-        },
-        {
-            "title": COMMAND_NAMES["volume_inc"],
-            "subTitle": "Increases volume by indicated amount",
-            "icoPath": "images/app.png",
-            "score": 1,
-            "jsonRPCAction": {
-                "method": "volume_inc",
-                "parameters": []
-            }
-        },
-        {
-            "title": COMMAND_NAMES["volume_dec"],
-            "subTitle": "Decreases volume by indicated amount",
-            "icoPath": "images/app.png",
-            "score": 1,
-            "jsonRPCAction": {
-                "method": "volume_dec",
-                "parameters": []
-            }
-        },
-        {
-            "title": COMMAND_NAMES["mute"],
-            "subTitle": "Mutes system volume",
-            "icoPath": "images/app.png",
-            "score": 1,
-            "jsonRPCAction": {
-                "method": "mute",
-                "parameters": []
-            }
-        }
-    ]
 
 class Nutils(FlowLauncher):
 
     def query(self, query: str) -> list:
-        if len(query) == 0:
-            return []
+        arguments = list(map(lambda x: x.strip(), query.lower().split(" ")))
+        query_data = []
+        for arg in arguments:
+            if arg != "":
+                query_data.append(arg)
         
-        return self.resolve_query(query.strip().lower())
+        COMMANDS = [
+            {
+                "title": COMMAND_NAMES["pause_or_resume_media"],
+                "subTitle": "Pause or resume playing media",
+                "icoPath": "images/app.png",
+                "score": 1,
+                "jsonRPCAction": {
+                    "method": "pause_or_resume_media",
+                    "parameters": []
+                }
+            },
+            {
+                "title": COMMAND_NAMES["next_media"],
+                "subTitle": "Plays next media if it is available",
+                "icoPath": "images/app.png",
+                "score": 1,
+                "jsonRPCAction": {
+                    "method": "next_media",
+                    "parameters": []
+                }
+            },
+            {
+                "title": COMMAND_NAMES["prev_media"],
+                "subTitle": "Plays previous media if it is available",
+                "icoPath": "images/app.png",
+                "score": 1,
+                "jsonRPCAction": {
+                    "method": "prev_media",
+                    "parameters": []
+                }
+            },
+            {
+                "title": COMMAND_NAMES["stop_media"],
+                "subTitle": "Stops playing media",
+                "icoPath": "images/app.png",
+                "score": 1,
+                "jsonRPCAction": {
+                    "method": "stop_media",
+                    "parameters": []
+                }
+            },
+            {
+                "title": COMMAND_NAMES["volume_inc"],
+                "subTitle": "Increases volume by indicated amount",
+                "icoPath": "images/app.png",
+                "score": 1,
+                "jsonRPCAction": {
+                    "method": "volume_inc",
+                    "parameters": [query_data[1]]
+                }
+            },
+            {
+                "title": COMMAND_NAMES["volume_dec"],
+                "subTitle": "Decreases volume by indicated amount",
+                "icoPath": "images/app.png",
+                "score": 1,
+                "jsonRPCAction": {
+                    "method": "volume_dec",
+                    "parameters": [query_data[1]]
+                }
+            },
+            {
+                "title": COMMAND_NAMES["mute"],
+                "subTitle": "Mutes system volume",
+                "icoPath": "images/app.png",
+                "score": 1,
+                "jsonRPCAction": {
+                    "method": "mute",
+                    "parameters": []
+                }
+            }
+        ]
 
-    def resolve_query(self, arg: str) -> list:        
+        if len(query) == 0:
+            return COMMANDS
+
+        return self.resolve_query(query.strip().lower(), commands)
+
+    def resolve_query(self, arg: str, commands: list) -> list:
         query_result = []
-        for command in COMMANDS:
+        for command in commands:
             if arg in command["title"]:
                 query_result.append(command)
-            
+
         return query_result
 
     def context_menu(self, data):
@@ -134,19 +142,30 @@ class Nutils(FlowLauncher):
 
     def pause_or_resume_media(self):
         pyautogui.press(KEYBOARD_MAPPING["pause/resume"])
-    
+
     def next_media(self):
         pyautogui.press(KEYBOARD_MAPPING["next"])
-    
+
     def prev_media(self):
         pyautogui.press(KEYBOARD_MAPPING["prev"], presses=2)
-    
+
     def stop_media(self):
         pyautogui.press(KEYBOARD_MAPPING["stop"])
 
+    def volume_inc(self, amount=1):
+        pyautogui.press(
+            KEYBOARD_MAPPING["volume_inc"], presses=ceil(amount / 2))
+
+    def volume_dec(self, amount=1):
+        pyautogui.press(
+            KEYBOARD_MAPPING["volume_dec"], presses=ceil(amount / 2))
+
+    def mute(self):
+        pyautogui.press(KEYBOARD_MAPPING["mute"])
 
     def open_url(self, url):
         webbrowser.open(url)
+
 
 if __name__ == "__main__":
     Nutils()
